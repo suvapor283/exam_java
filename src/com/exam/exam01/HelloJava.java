6d53469d2e65e735e67d49e4dfe125f8a83da798
suvapor283 <suvapor8632@gmail.com>
2024-07-02 23:18:01 +0900

    exam01

---
+++ src/com/exam/exam01/HelloJava.java
@@ -0,0 +1,34 @@
+package com.exam.exam01;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		
+		// 변수 선언
+		
+		// 정수형
+		int num01 = 10;
+		byte byNum = 20;
+		
+		// 실수
+		double doubleNum = 30.3111;
+		float flNum = 33.12f; // float는 값 뒤에 f를 붙여줘야 한다.
+		
+		// 문자
+		char ch = 'A';
+		
+		// 문자열
+		String str = "날씨";
+		
+		// 논리형
+		boolean b = false;
+		
+		System.out.println(num01);
+		System.out.println(byNum);
+		System.out.println(doubleNum);
+		System.out.println(flNum);
+		System.out.println(ch);
+		System.out.println(str);
+		System.out.println(b);
+	}
+}
