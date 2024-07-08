1a5b573a0e12924c4487c23c12fc7beeefa3cfe0
suvapor283 <suvapor8632@gmail.com>
2024-07-08 22:18:34 +0900

    exam15

---
+++ src/com/exam/exam15/Car.java
@@ -0,0 +1,13 @@
+package com.exam.exam15;
+
+public class Car {
+
+	// 필드
+	int year;
+	String carName = "소나타";	// 클래스의 멤버변수
+	
+	// 메서드
+	public void go() {
+		System.out.println(carName + "이 출발합니다.");
+	}
+}
---
+++ src/com/exam/exam15/CarMain.java
@@ -0,0 +1,13 @@
+package com.exam.exam15;
+
+public class CarMain {
+	
+	public static void main(String[] args) {
+		
+		Car car = new Car();
+		
+		car.year = 2020;
+		car.carName = "소나타";
+		car.go();
+	}
+}
